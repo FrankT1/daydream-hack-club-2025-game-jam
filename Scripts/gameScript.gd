01f1308b6e86ed7d70a037684fe07f80859174d1
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2025-09-27 13:38:19 -0700

    Add damage

--- Scripts/gameScript.gd
+++ Scripts/gameScript.gd
@@ -4,10 +4,11 @@ extends Node
 @export var snakeLen = 1
 @export var gridSize = 128
 @export var health = 100
-
+@export var score = 0
 
 @export var player: Node2D
 
+var mob = "res://Assets/mob.tscn"
 
 
 func move(dir : Vector2):
@@ -28,12 +29,30 @@ func gameTick():
 	elif snakeDir == 3:
 		move(Vector2(-1, 0))
 
+func spawnTick():
+	score += 10
+	var newmob : Node2D = preload("res://Assets/mob.tscn").instantiate()
+	$game/Mobs.add_child(newmob)
+	var dir = [-1, 1].pick_random()
+	newmob.position = Vector2(dir * 5 * gridSize, randi_range(-4, 4) * gridSize)
+	var tw = create_tween().tween_property(newmob, "position", Vector2(gridSize * 8 * dir * -1, newmob.position.y), randf_range(2, 10))
+	tw.finished.connect(func():
+		newmob.queue_free()
+		)
+	
+	
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$CanvasLayer/Control/Label.text = "Health: " + str(health)
+	$CanvasLayer/Control/Health.text = "Health: " + str(health)
+	$CanvasLayer/Control/Score.text = "Score: " + str(score)
+	if abs(player.position.x) > 600 || abs(player.position.y) > 600:
+		health = 0
 	if health <= 0:
 		end()
+	
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Up"):
@@ -51,7 +70,7 @@ func _input(event: InputEvent) -> void:
 	
 func _ready() -> void:
 	$Tick.timeout.connect(gameTick)
+	$SpawnTick.timeout.connect(spawnTick)
 	$game/Player/Area2D.area_entered.connect(func(area : Area2D):
 		health -= 10
-	
 	)
