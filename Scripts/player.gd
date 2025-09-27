cf65f2f9de2bd0f7c5e58bbf8769392840c8d7e5
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2025-09-27 11:20:45 -0700

    Add input direction

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,27 @@
-extends StaticBody2D
+extends Node
+
+@export var snakeDir = 0 # 0 up 1 right, etc.
 
 
 func gameTick():
 	pass	
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Tick.timeout.connect(gameTick)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Up"):
+		snakeDir = 0
+	elif event.is_action_pressed("Right"):
+		snakeDir = 1
+	elif event.is_action_pressed("Down"):
+		snakeDir = 2
+	elif event.is_action_pressed("Left"):
+		snakeDir = 3
+	print(snakeDir)
+	
+	
+func _ready() -> void:
+	$Tick.timeout.connect(gameTick)
